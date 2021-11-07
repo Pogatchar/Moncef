2445f6180b110ce674b40dde2f65dffc70090a4e
Pogatchar <86927351+Pogatchar@users.noreply.github.com>
2021-11-07 10:11:38 +0100

    commentaire dans single page Home
    // ceci est une capture d'écran de ce qui va être ici ( animation vue js ) 
    // https://github.com/Pogatchar/LandingGlobe

--- src/views/app/single/index.vue
+++ src/views/app/single/index.vue
@@ -15,6 +15,8 @@
               <i class="simple-icon-pencil" />
             </b-button>
           </div>
+          // ceci est une capture d'écran de ce qui va être ici ( animation vue js ) 
+          // https://github.com/Pogatchar/LandingGlobe
           <img
             src="/assets/img/Globe/Linkedin.png"
             alt="Detail"
